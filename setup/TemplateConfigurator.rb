d5acda52dc69cd213d6fd8745c3e261fec20336e
Leonir Alves <leonir.alves@qranio.com>
2016-07-11 16:59:55 -0300

    customização do pod-template

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -117,7 +117,7 @@ module Pod
     end
 
     def replace_variables_in_files
-      file_names = ['POD_LICENSE', 'POD_README.md', 'NAME.podspec', podfile_path]
+      file_names = ['POD_LICENSE', 'POD_README.md', 'NAME.podspec', '.travis.yml', podfile_path]
       file_names.each do |file_name|
         text = File.read(file_name)
         text.gsub!("${POD_NAME}", @pod_name)
@@ -167,7 +167,7 @@ module Pod
 
     def rename_template_files
       FileUtils.mv "POD_README.md", "README.md"
-      FileUtils.mv "POD_LICENSE", "LICENSE_QRANIO"
+      FileUtils.mv "POD_LICENSE", "LICENSE"
       FileUtils.mv "NAME.podspec", "#{pod_name}.podspec"
     end
 
